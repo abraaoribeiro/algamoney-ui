2f4c90abb25c361faeaf084a4a142f8011182991
Abraao Ribeiro <abraaohdg@gmail.com>
2020-07-04 11:13:04 -0300

    ajuste na interceptação do token

--- src/app/seguranca/money-http-interceptor.ts
+++ src/app/seguranca/money-http-interceptor.ts
@@ -12,21 +12,18 @@ export class MoneyHttpInterceptor implements HttpInterceptor {
 
   constructor(private auth: AuthService) { }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (!req.url.includes('/oauth/token') && this.auth.isAccessTokenInvalido()) {
-      return from(this.auth.obterNovoAccesToken())
-        .pipe(mergeMap(() => {
-          if (this.auth.isAccessTokenInvalido()) {
-            throw new NotAuthenticatedError();
-          }
-          req = req.clone({
-            setHeaders: {
-              Authorization: `Bearer ${localStorage.getItem('token')}`
-            },
-          });
-          return next.handle(req);
-        }))
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = localStorage.getItem('token');
+
+    if (token && request.url.indexOf('/oauth/token') === -1) {
+      request = request.clone({
+        setHeaders: {
+          Accept: `application/json`,
+          'Content-Type': `application/json`,
+          Authorization: `Bearer ${token}`
+        }
+      });
     }
-    return next.handle(req);
+    return next.handle(request);
   }
 }    
